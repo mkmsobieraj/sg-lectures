8030b20ca2de723a8e93f8dcdb57b2131a9dc29d
mkmsobieraj <m.k.m.sobieraj@gmail.com>
2017-03-13 22:44:23 +0100

    Add files via upload
    Add first lecture of series and all necessary dependencies.

---
+++ exampleI.R
@@ -0,0 +1,39 @@
+# `F1 (kursor na funkcji)` - wyświetlenie pomocy
+mean()
+
+# `F2 (kursor na funkcji)` - wyświetlenie kodu funkcji 
+lm()
+
+# `Crtl + Enter` - wykonanie linii/fragmentu kodu
+for (i in 1:10) {
+  print(paste("iteration ", i))
+}
+
+# `Ctrl + Shift + C` - za komentowanie / od komentowanie linii lub zaznaczonego obszaru
+if (TRUE) {
+  print("do sth important")
+} else{
+  print("do nothing")
+}
+
+# `Alt + -` - wstawienie strzałki przypisania `<-`
+var_14
+
+
+# `Crtl + 1` / `Crtl + 2` przejście do skryptu/konsoli
+
+# `Crtl + l` - czyszczenie konsoli
+
+# `Tab` - podczas pisania funkcji podpowiedź
+me
+
+# `Tab` - podczas wpisywania argumentów podpowiedź
+lm()
+
+# `Tab` - podczas gdy zaznaczone jest kilka linii, dodanie wcięcia
+# `Tab + Shift` - podczas gdy zaznaczone jest kilka linii, usunięcie wcięcia
+if (TRUE) {
+  print("do sth important")
+} else{
+  print("do nothing")
+}
